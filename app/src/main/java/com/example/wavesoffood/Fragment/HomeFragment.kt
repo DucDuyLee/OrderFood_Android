4c84873ee202191c09723c7ea28dc2e65ce755ce
DucDuyLee <duyminecraft77@gmail.com>
2024-05-19 20:02:57 +0700

    Duy Commit For Project

--- app/src/main/java/com/example/wavesoffood/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/wavesoffood/Fragment/HomeFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 import com.example.wavesoffood.R
+import com.example.wavesoffood.adapter.PopularAdapter
 import com.example.wavesoffood.databinding.FragmentHomeBinding
 
 
@@ -42,6 +46,24 @@ class HomeFragment : Fragment() {
         val imageSlider = binding.imageSlider
         imageSlider.setImageList(imageList)
         imageSlider.setImageList(imageList, ScaleTypes.FIT)
+        imageSlider.setItemClickListener(object :ItemClickListener{
+            override fun doubleClick(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(position: Int) {
+                val itemPosition = imageList[position]
+                val itemMessage = "Selected Image $position"
+                Toast.makeText(requireContext(),itemMessage,Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        val foodName = listOf("Burger", "sandwich", "momo", "item")
+        val Price = listOf("$5", "$7", "$8", "$10")
+        val populerFoodImages = listOf(R.drawable.menu1, R.drawable.menu2, R.drawable.menu3, R.drawable.menu4)
+        val adapter = PopularAdapter(foodName, Price, populerFoodImages)
+        binding.PopulerRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.PopulerRecyclerView.adapter = adapter
     }
 
     companion object {
